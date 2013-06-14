1a9e8aea243e491d6de473fe928b1f9fc39e09ca
arnaub <abesorasans@gmail.com>
2013-06-14 10:46:10 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Athome::Application.config.secret_token = '31ece66b7ffb1c06db47b5b741b25a8f96cef6028504c22f714eb706b992719c71ae33f033be812dfbb9e95cb7f5e6a000c8b19068e08797c28db1c9fdf4188e'
